f4b1d20f5ffd3df5a6c4fa4d397f09ba99deda62
stuhops <stuart.hopkins@aggiemail.usu.edu>
2020-02-26 18:47:28 -0700

    Abstracted the calcNeighbors

--- serial.cpp
+++ serial.cpp
@@ -21,6 +21,46 @@ void printBoard(int arr[], const int ROW_SIZE) {
   cout << endl;
 }
 
+
+int calcNeighbors(int arr[], const int ROW_SIZE, int i, int j) {
+  int neighbors = 0;
+
+  //north 3
+  if(i != 0) {
+    // Upper left
+    if(j != 0)
+      neighbors += arr[ROW_SIZE * (i-1) + (j-1)];
+    // Upper
+    neighbors += arr[ROW_SIZE * (i-1) + j];
+    // Upper right
+    if(j+1 < ROW_SIZE)
+      neighbors += arr[ROW_SIZE * (i-1) + (j+1)];
+  }
+
+  // Middle 2
+  // Left
+  if(j != 0)
+    neighbors += arr[ROW_SIZE * (i) + (j-1)];
+  // Right
+  if(j+1 < ROW_SIZE)
+    neighbors += arr[ROW_SIZE * (i) + (j+1)];
+
+  // South 3 
+  if(i+1 < ROW_SIZE) {
+    // Upper left
+    if(j != 0)
+      neighbors += arr[ROW_SIZE * (i+1) + (j-1)];
+    // Upper
+    neighbors += arr[ROW_SIZE * (i+1) + j];
+    // Upper right
+    if(j+1 < ROW_SIZE)
+      neighbors += arr[ROW_SIZE * (i+1) + (j+1)];
+  }
+
+  return neighbors;
+}
+
+
 int main(int argc, char **argv) {
   const int ROW_SIZE = 8;
   int arr[ROW_SIZE * ROW_SIZE] = {0,0,0,0,0,0,0,0,
@@ -44,41 +84,7 @@ int main(int argc, char **argv) {
   for(int cnt = 0; cnt < 3; cnt++){
     for(int i = 0; i < ROW_SIZE; ++i) {
       for(int j = 0; j < ROW_SIZE; ++j) {
-        int neighbors = 0;
-
-        //north 3
-        if(i != 0) {
-          // Upper left
-          if(j != 0)
-            neighbors += arr[ROW_SIZE * (i-1) + (j-1)];
-          // Upper
-          neighbors += arr[ROW_SIZE * (i-1) + j];
-          // Upper right
-          if(j+1 < ROW_SIZE)
-            neighbors += arr[ROW_SIZE * (i-1) + (j+1)];
-        }
-
-        // Middle 2
-        // Left
-        if(j != 0)
-          neighbors += arr[ROW_SIZE * (i) + (j-1)];
-        // Right
-        if(j+1 < ROW_SIZE)
-          neighbors += arr[ROW_SIZE * (i) + (j+1)];
-
-        // South 3 
-        if(i+1 < ROW_SIZE) {
-          // Upper left
-          if(j != 0)
-            neighbors += arr[ROW_SIZE * (i+1) + (j-1)];
-          // Upper
-          neighbors += arr[ROW_SIZE * (i+1) + j];
-          // Upper right
-          if(j+1 < ROW_SIZE)
-            neighbors += arr[ROW_SIZE * (i+1) + (j+1)];
-
-        }
-
+        int neighbors = calcNeighbors(arr, ROW_SIZE, i, j);
 
         if(neighbors == 0 || neighbors == 1)
           tmpArr[ROW_SIZE * i + j] = 0;
@@ -86,7 +92,6 @@ int main(int argc, char **argv) {
           tmpArr[ROW_SIZE * i + j] = 1;
         else if(neighbors > 3)
           tmpArr[ROW_SIZE * i + j] = 0;
-
       }
     }
 
@@ -97,7 +102,6 @@ int main(int argc, char **argv) {
     }
     printBoard(arr, ROW_SIZE);
   }
-
 
   return 0;
 }
